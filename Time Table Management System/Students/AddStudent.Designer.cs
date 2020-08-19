8cfe0cdecafdb1adb7feadf42b188775d84b6412
Viraj Thanuja Konthasinghe <it18145526@my.sliit.lk>
2020-08-20 00:43:54 +0530

    Changes for Student

--- Time Table Management System/Students/AddStudent.Designer.cs
+++ Time Table Management System/Students/AddStudent.Designer.cs
@@ -28,22 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.title = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxYearSem = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxProgramme = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxGroupNo = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxGroupID = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBoxSubGroupNo = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBoxSubGroupID = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.btngenerateIDs = new System.Windows.Forms.Button();
+            this.errorStudent = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorStudent)).BeginInit();
             this.SuspendLayout();
             // 
             // title
@@ -88,15 +92,15 @@
             this.label1.Text = "Academic Year & Semester";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // textBoxYearSem
             // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(289, 109);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(219, 27);
-            this.textBox1.TabIndex = 15;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBoxYearSem.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxYearSem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxYearSem.Location = new System.Drawing.Point(289, 109);
+            this.textBoxYearSem.Name = "textBoxYearSem";
+            this.textBoxYearSem.Size = new System.Drawing.Size(219, 27);
+            this.textBoxYearSem.TabIndex = 15;
+            this.textBoxYearSem.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
@@ -111,21 +115,21 @@
             this.label2.TabIndex = 16;
             this.label2.Text = "Programme";
             // 
-            // comboBox1
+            // comboBoxProgramme
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.comboBoxProgramme.FormattingEnabled = true;
+            this.comboBoxProgramme.Items.AddRange(new object[] {
             "IT",
             "CSSE",
             "CSE",
             "DS",
             "CN"});
-            this.comboBox1.Location = new System.Drawing.Point(299, 169);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(219, 24);
-            this.comboBox1.TabIndex = 17;
-            this.comboBox1.Tag = "";
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBoxProgramme.Location = new System.Drawing.Point(299, 169);
+            this.comboBoxProgramme.Name = "comboBoxProgramme";
+            this.comboBoxProgramme.Size = new System.Drawing.Size(219, 24);
+            this.comboBoxProgramme.TabIndex = 17;
+            this.comboBoxProgramme.Tag = "";
+            this.comboBoxProgramme.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label3
             // 
@@ -140,15 +144,15 @@
             this.label3.TabIndex = 18;
             this.label3.Text = "Group Number";
             // 
-            // textBox2
+            // textBoxGroupNo
             // 
-            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(289, 202);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(219, 27);
-            this.textBox2.TabIndex = 19;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBoxGroupNo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxGroupNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxGroupNo.Location = new System.Drawing.Point(289, 213);
+            this.textBoxGroupNo.Name = "textBoxGroupNo";
+            this.textBoxGroupNo.Size = new System.Drawing.Size(219, 27);
+            this.textBoxGroupNo.TabIndex = 19;
+            this.textBoxGroupNo.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label4
             // 
@@ -157,21 +161,22 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.Info;
-            this.label4.Location = new System.Drawing.Point(54, 251);
+            this.label4.Location = new System.Drawing.Point(73, 325);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(77, 20);
             this.label4.TabIndex = 20;
             this.label4.Text = "Group ID";
             // 
-            // textBox3
+            // textBoxGroupID
             // 
-            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(289, 251);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(219, 27);
-            this.textBox3.TabIndex = 21;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBoxGroupID.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxGroupID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxGroupID.Location = new System.Drawing.Point(289, 325);
+            this.textBoxGroupID.Name = "textBoxGroupID";
+            this.textBoxGroupID.ReadOnly = true;
+            this.textBoxGroupID.Size = new System.Drawing.Size(219, 27);
+            this.textBoxGroupID.TabIndex = 21;
+            this.textBoxGroupID.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label5
             // 
@@ -180,21 +185,21 @@
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.Info;
-            this.label5.Location = new System.Drawing.Point(54, 301);
+            this.label5.Location = new System.Drawing.Point(63, 265);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(153, 20);
             this.label5.TabIndex = 22;
             this.label5.Text = "Sub Group Number";
             // 
-            // textBox4
+            // textBoxSubGroupNo
             // 
-            this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(289, 301);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(219, 27);
-            this.textBox4.TabIndex = 23;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.textBoxSubGroupNo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxSubGroupNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSubGroupNo.Location = new System.Drawing.Point(289, 265);
+            this.textBoxSubGroupNo.Name = "textBoxSubGroupNo";
+            this.textBoxSubGroupNo.Size = new System.Drawing.Size(219, 27);
+            this.textBoxSubGroupNo.TabIndex = 23;
+            this.textBoxSubGroupNo.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // label6
             // 
@@ -203,28 +208,29 @@
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.Info;
-            this.label6.Location = new System.Drawing.Point(54, 358);
+            this.label6.Location = new System.Drawing.Point(73, 365);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(111, 20);
             this.label6.TabIndex = 24;
             this.label6.Text = "Sub Group ID";
             // 
-            // textBox5
+            // textBoxSubGroupID
             // 
-            this.textBox5.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(289, 355);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(219, 27);
-            this.textBox5.TabIndex = 25;
-            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            this.textBoxSubGroupID.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxSubGroupID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSubGroupID.Location = new System.Drawing.Point(289, 373);
+            this.textBoxSubGroupID.Name = "textBoxSubGroupID";
+            this.textBoxSubGroupID.ReadOnly = true;
+            this.textBoxSubGroupID.Size = new System.Drawing.Size(219, 27);
+            this.textBoxSubGroupID.TabIndex = 25;
+            this.textBoxSubGroupID.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button3.Location = new System.Drawing.Point(579, 339);
+            this.button3.Location = new System.Drawing.Point(592, 365);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(120, 40);
             this.button3.TabIndex = 26;
@@ -237,7 +243,7 @@
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(718, 339);
+            this.button1.Location = new System.Drawing.Point(718, 365);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(120, 40);
             this.button1.TabIndex = 27;
@@ -245,31 +251,50 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btngenerateIDs
+            // 
+            this.btngenerateIDs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btngenerateIDs.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btngenerateIDs.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btngenerateIDs.Location = new System.Drawing.Point(605, 231);
+            this.btngenerateIDs.Name = "btngenerateIDs";
+            this.btngenerateIDs.Size = new System.Drawing.Size(200, 40);
+            this.btngenerateIDs.TabIndex = 28;
+            this.btngenerateIDs.Text = "Generate IDs";
+            this.btngenerateIDs.UseVisualStyleBackColor = true;
+            this.btngenerateIDs.Click += new System.EventHandler(this.btnGenerateIDs_Click);
+            // 
+            // errorStudent
+            // 
+            this.errorStudent.ContainerControl = this;
+            // 
             // AddStudent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Desktop;
             this.ClientSize = new System.Drawing.Size(881, 442);
+            this.Controls.Add(this.btngenerateIDs);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBoxSubGroupID);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBoxSubGroupNo);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBoxGroupID);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxGroupNo);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBoxProgramme);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxYearSem);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.title);
             this.ForeColor = System.Drawing.SystemColors.Info;
             this.Name = "AddStudent";
             this.Text = "AddStudent";
+            ((System.ComponentModel.ISupportInitialize)(this.errorStudent)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -279,19 +304,20 @@
         private System.Windows.Forms.Label title;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxYearSem;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxProgramme;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxGroupNo;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBoxGroupID;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBoxSubGroupNo;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBoxSubGroupID;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ErrorProvider errorAddStudent;
+        private System.Windows.Forms.Button btngenerateIDs;
+        private System.Windows.Forms.ErrorProvider errorStudent;
     }
 }
